4e859ff60e50725e1a3cfe84bf2f12a34f7007ad
andrew shin <shinandy22@gmail.com>
2025-06-17 18:07:17 -0700

    deploy err fix4

--- backend/index.js
+++ backend/index.js
@@ -24,10 +24,10 @@ app.use("/api/recipes", recipeRoute);
 app.use("/api/auth", authRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get(/^(?!\/api).*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
